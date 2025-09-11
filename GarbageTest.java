5007cbd7b545f5e8071d1108db8930170e8a3af2
Naganjali11 <naganjali425@gmail.com>
2025-09-11 22:00:54 +0530

    javafullstackprograms

---
+++ GarbageTest.java
@@ -0,0 +1,17 @@
+package com.codegnan.oopexamples;
+
+public class GarbageTest {
+	public void finalize() {
+		System.out.println("Garbage collector called finalize() method");
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GarbageTest obj=new GarbageTest();
+		obj=null;
+	System.out.println("Requesting the garbage collector");
+	System.gc();
+	System.out.println("End of the Mian Method");
+
+	}
+
+}
