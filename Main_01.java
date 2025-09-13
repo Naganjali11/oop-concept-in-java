f6f438402e5b27ec9c40974124743afca5aa69f5
Naganjali11 <naganjali425@gmail.com>
2025-09-13 22:11:51 +0530

    javafullstackprograms

---
+++ Main_01.java
@@ -0,0 +1,16 @@
+package com.codegnan.oopexamples;
+
+public class Main_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Employee e1=new Employee();
+		e1.displayInfo();
+		Employee e2=new Employee(101,"Malli");
+		e2.displayInfo();*/
+		Employee e3=new Employee(102,"Arjun","Training");
+		e3.displayInfo();
+
+	}
+
+}
