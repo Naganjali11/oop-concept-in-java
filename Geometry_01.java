3ebae19dbfd26bb41eed270c00f9792f2653de0a
Naganjali11 <naganjali425@gmail.com>
2025-09-17 16:39:52 +0530

    javafullstackprograms

---
+++ Geometry_01.java
@@ -0,0 +1,29 @@
+package com.codegnan.oopexamples;
+
+public class Geometry_01 {
+	
+		   double length,width;
+		   double radius;
+		   double side;
+		   double base, height;
+		   
+		   static final double pi=3.14159;
+		   
+		   public static double calculateRectangleArea(double length,double width) {
+				return length*width;
+				
+			}
+		   
+		   public static double calculateCircleArea(double radius) {
+				return pi*radius*radius;
+			}
+		   
+		   public static double calculatesquareArea(double side) {
+			   return side * side;
+		   }
+		   
+		   public static double calculateTriangleArea(double base, double height) {
+			    return 0.5 * base * height;
+			}
+
+}
