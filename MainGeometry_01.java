f653b400e3f83792c3903ee477fff896a7de1670
Naganjali11 <naganjali425@gmail.com>
2025-09-17 16:40:14 +0530

    javafullstackprograms

---
+++ MainGeometry_01.java
@@ -0,0 +1,15 @@
+package com.codegnan.oopexamples;
+
+public class MainGeometry_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Rectangle Area : "+Geometry_01.calculateRectangleArea(6,4));
+		System.out.println("Area of circle : "+Geometry_01.calculateCircleArea(7));
+		System.out.println("Area of square : "+Geometry_01.calculatesquareArea(5));
+		System.out.println("Area of triangle : "+Geometry_01.calculateTriangleArea(3,4));
+
+
+	}
+
+}
