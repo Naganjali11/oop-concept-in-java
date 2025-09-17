d87d45c5be2d883196c46db575a14fc3feef80c1
Naganjali11 <naganjali425@gmail.com>
2025-09-17 16:40:38 +0530

    javafullstackprograms

---
+++ FactorialUsingRecurssion.java
@@ -0,0 +1,18 @@
+package com.codegnan.oopexamples;
+
+public class FactorialUsingRecurssion {
+
+	
+    public static int factorial(int n) {
+    	if(n==0) {
+    		return 1;
+    	}
+    	return n*factorial(n-1);//5*4*3*2*1*1
+    }
+	public static void main(String[] args) {
+		System.out.println(factorial(5));
+	}
+
+
+
+}
