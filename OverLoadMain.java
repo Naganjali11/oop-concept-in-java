6337e3d9d3a2f7377363734d5e1ea9b82c769f76
Naganjali11 <naganjali425@gmail.com>
2025-09-18 22:07:12 +0530

    javafullstackprograms

---
+++ OverLoadMain.java
@@ -0,0 +1,17 @@
+package com.codegnan.oopexamples;
+
+public class OverLoadMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OverLoadDemo o1=new OverLoadDemo();
+		o1.test();
+		o1.test(10);
+		o1.test(20,30);
+		System.out.println(o1.test(12.5));
+		
+		
+
+	}
+
+}
