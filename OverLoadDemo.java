a504434b5b9dca29591d6cff074c6153c40ee955
Naganjali11 <naganjali425@gmail.com>
2025-09-18 22:06:38 +0530

    javafullstackprograms

---
+++ OverLoadDemo.java
@@ -0,0 +1,17 @@
+package com.codegnan.oopexamples;
+
+public class OverLoadDemo {
+	public void test() {
+		System.out.println("No parameters");
+	}
+	public void test(int a) {
+		System.out.println("a:"+a);
+	}
+	public void test(int a,int b) {
+		System.out.println("A and B is:"+a+":"+b);
+	}
+	public double test(double a) {
+		System.out.println("Double a:"+a);
+		return a*a;
+	}
+}
