b8e09b3df2ca6ee7d1c9450b859b7cfd178684a2
Naganjali11 <naganjali425@gmail.com>
2025-09-19 21:38:14 +0530

    javafullstackprograms

---
+++ Employee3Main.java
@@ -0,0 +1,18 @@
+package com.codegnan.oopexamples;
+
+public class Employee3Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee3 e1=new Employee3(124,"Amrutha",20000);
+	     e1.DisplayInfo();
+	     e1.setEmployeeId(156);
+	     e1.setName("Arun");
+	     e1.setSalary(-40000);
+		e1.DisplayInfo();
+		}
+
+
+	}
+
+
