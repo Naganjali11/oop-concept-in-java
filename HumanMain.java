7a7b059a788472d3ea89e7f24debda9a4f74e959
Naganjali11 <naganjali425@gmail.com>
2025-09-19 21:24:14 +0530

    javafullstackprograms

---
+++ HumanMain.java
@@ -0,0 +1,18 @@
+package com.codegnan.oopexamples;
+
+public class HumanMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				    Human human1 = new Human();
+					human1.setName("Naganjali");
+					human1.setAge(20);
+					System.out.println("Name : " +human1.getName()  +  " age : " +human1.getAge());
+			}
+
+		
+
+	}
+
+
