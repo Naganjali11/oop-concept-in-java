991efb6f915abf294974b804211765f293ed20f2
Naganjali11 <naganjali425@gmail.com>
2025-09-19 21:31:33 +0530

    javafullstackprograms

---
+++ CarMain.java
@@ -0,0 +1,14 @@
+package com.codegnan.oopexamples;
+
+public class CarMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1 = new Car();
+		car1.setModel("Camry");
+		car1.setYear(2025);
+		System.out.println("Name : " +car1.getModel()  +  " year : " +car1.getyear());
+
+	}
+
+}
